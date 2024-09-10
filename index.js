d58def709d0df2f3b67187cfb0e89c18aed0e4c4
Xavier <128425706+xcg2003@users.noreply.github.com>
2024-09-09 23:13:36 -0700

    saved the data to the server side

--- index.js
+++ index.js
@@ -11,13 +11,33 @@ const __dirname = path.dirname(__filename);
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middle ware to parse the request body
+app.use(express.json());
+
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
+// array of messages
+let pageMessages = [];
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'chat.html'));
     res.status(200);
 });
 
-app.get("/chat", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// TODO: post does not save right now
+app.post('/', (req, res) => {
+    const username = req.body.username;
+    const message = req.body.message;
+    if(!username || !message) {
+        return res.status(400).send("Username and message are required");
+    }
+    pageMessages.push({ username, message });
+    res.redirect('/chatpage');
+});
+
+app.get("/chatpage", (req, res) => {
+    res.send(pageMessages);
     res.status(200);
 });
 
--- public/chat.js
+++
@@ -1,5 +0,0 @@
-function getChatFromUser(){
-    var user = document.getElementById("username").value;
-    var chat = document.getElementById("message").value;
-    
-}
