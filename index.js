02cd62f3a32b191fcd231332c0984394cbc38664
Xavier <128425706+xcg2003@users.noreply.github.com>
2024-09-09 22:07:37 -0700

    created public files, linked / to chat.html on server

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+    res.status(200);
+});
+
+app.get("/chat", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
     res.status(200);
 });
 
